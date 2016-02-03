cd05ab686c06e1c74e95d09c863754069f6853b1
chenyufeng <503434355@qq.com>
2016-02-03 19:51:33 +0800

    complete

--- SelectSort/main.c
+++ SelectSort/main.c
@@ -8,8 +8,40 @@
 
 #include <stdio.h>
 
+void selectSort(int *a,int n);
+void swap(int *a,int *b);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+
+    int a[] = {6,1,4,9,0,3};
+    selectSort(a,6);
+    for (int i = 0; i < 6 ; i++) {
+        printf("%d ",a[i]);
+    }
+
     return 0;
+}
+
+void selectSort(int *a,int n){
+
+    int i,j,minIndex;
+    for (i = 0; i < n; i++) {
+
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(&a[i],&a[minIndex]);
+    }
+}
+
+void swap(int *a,int *b){
+
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
